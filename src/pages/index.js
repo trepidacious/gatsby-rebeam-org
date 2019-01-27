50db5ab890965411275e96b043b64e88bb504ce3
trepidacious <trepidacious@gmail.com>
2019-01-27 15:10:54 +0000

    Better formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import { Segment } from "semantic-ui-react";
+import { Segment, Label } from "semantic-ui-react";
 
 export default ({ data }) => {
   return (
     <Layout description="Rebeam blog - Scala, tree data system, notes" keywords="Scala, React, Tree, Blog">
       
       <h2>
-        Recent Blogs
+        Recent Entries
       </h2>
 
       {data.allMarkdownRemark.edges.map(({ node }) => (
@@ -17,11 +17,12 @@ export default ({ data }) => {
         <Segment vertical key={node.id}>
           <h3>
             <Link to={node.fields.slug}>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </Link>
+              {node.frontmatter.title}
+            </Link> 
           </h3>
           <p>
-            {node.excerpt}
+          <Label horizontal basic>{node.frontmatter.date}</Label>
+             {node.excerpt}
           </p>
         </Segment>
 
