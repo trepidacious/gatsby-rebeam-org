10964ffb842aa216dcd7ca73d027ef0bcb054a6c
trepidacious <trepidacious@gmail.com>
2019-02-14 23:08:39 +0000

    Fix footer layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import { Container, Grid, Segment, List, Header } from 'semantic-ui-react'
+import { Container, Grid, Segment } from 'semantic-ui-react'
 
 import Nav from './header'
 import BlogList from './blog-list'
 
 import 'semantic-ui-less/semantic.less'
 
+//See https://css-tricks.com/couple-takes-sticky-footer/ for footer layout, requires this
+//component to be in a 100% height parent, see site.overrides for styles to achieve this
 const Layout = ({ noBlogList, description, keywords, children }) => (
   <StaticQuery
     query={graphql`
@@ -21,7 +23,9 @@ const Layout = ({ noBlogList, description, keywords, children }) => (
       }
     `}
     render={data => (
-      <>
+
+      
+      <div style={{ display:"flex", height:"100%", flexDirection:"column" }}>
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -30,44 +34,37 @@ const Layout = ({ noBlogList, description, keywords, children }) => (
           ]}
         />
 
-        <div style={{ display:"flex", minHeight:"100vh", flexDirection:"column" }}>
 
-          <Nav siteTitle={data.site.siteMetadata.title} />
+        <Nav siteTitle={data.site.siteMetadata.title} />
 
-          <div style={{ flex:1 }}>
-            
-            <Container style={{paddingTop: '80px'}}>
-              <Grid centered>
-                <Grid.Row>
-                  <Grid.Column mobile={16} tablet={noBlogList ? 14 : 10} computer={noBlogList ? 14: 10}>
-                    {children}
-                  </Grid.Column>
-                  {!noBlogList && <Grid.Column only='tablet computer' tablet={4} computer={4}>
-                    <BlogList></BlogList>
-                  </Grid.Column>}
-                </Grid.Row>
-                {/* <Grid.Row>
-                  <Grid.Column mobile={16} tablet={14} computer={14}>
-                    Footer
-                  </Grid.Column>
-                </Grid.Row> */}
-              </Grid>
-            </Container>
-
-          </div>
-
-          <Segment inverted vertical style={{ padding: '1.5em 0', marginTop: '3em' }} className='theme-dark-grey'>
-            <Container>
-              <Grid inverted stackable centered>
-                <Grid.Column mobile={16} tablet={14} computer={14}>
-                  {/* Footer */}
+        <div style={{ flex:'1 0 auto' }}>
+          
+          <Container style={{paddingTop: '80px'}}>
+            <Grid centered>
+              <Grid.Row>
+                <Grid.Column mobile={16} tablet={noBlogList ? 14 : 10} computer={noBlogList ? 14: 10}>
+                  {children}
                 </Grid.Column>
-              </Grid>
-            </Container>
-          </Segment>
+                {!noBlogList && <Grid.Column only='tablet computer' tablet={4} computer={4}>
+                  <BlogList></BlogList>
+                </Grid.Column>}
+              </Grid.Row>
+            </Grid>
+          </Container>
 
         </div>
-      </>
+
+        <Segment inverted vertical style={{ padding: '1.5em 0', marginTop: '3em' }} className='theme-dark-grey'>
+          <Container>
+            <Grid inverted stackable centered>
+              <Grid.Column mobile={16} tablet={14} computer={14}>
+                {/* Footer */}
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </Segment>
+
+      </div>
     )}
   />
 )
