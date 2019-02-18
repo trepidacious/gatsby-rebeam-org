4153e48b1b0be07b90c4f39ee04634e457331a9a
trepidacious <trepidacious@gmail.com>
2019-02-18 22:18:00 +0000

    Fix hreo on mobile

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,15 @@
 import React from "react"
 
+import logo from "../images/icons/icon.svg"
 import hero from "../images/hero.svg"
-import { Container, Header } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 
 export default ()  => (
   <div className='hero'>
-    <img src={hero} alt='Hero' width='500px' height='333px' style={{position: 'absolute', right: '0', top: '57px'}}></img>
+    <img className='hero-svg-bg' src={hero} alt='Hero' width='500px' height='333px' style={{position: 'absolute', right: '0', top: '57px'}}></img>
     <Container>
       <h1 className='hero-h1'>
+        <img className='hero-logo' src={logo} alt='Logo' width='60px' height='60px' />
         rebeam.org
       </h1>
 
