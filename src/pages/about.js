c8843a8d39d80eac41d3148aa49dd7b21f334632
trepidacious <trepidacious@gmail.com>
2019-02-19 21:25:20 +0000

    Fix blog link - not "on the right" on mobile...

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
 import SmartLink from "../components/smart-link"
-import { Icon } from "semantic-ui-react";
 
 export default ({ data }) => (
   <Layout>
@@ -10,7 +9,7 @@ export default ({ data }) => (
       rebeam.org is hosted on <SmartLink href='https://www.netlify.com/'>Netlify</SmartLink>, automatically deploying from <SmartLink href='https://github.com/trepidacious/gatsby-rebeam-org'>Github</SmartLink>. It is built as a static site by <SmartLink href='https://www.gatsbyjs.org/'>Gatsby</SmartLink> and several Gatsby plugins, using <SmartLink href='https://semantic-ui.com/'>Semantic UI</SmartLink> styling via <SmartLink href='https://react.semantic-ui.com/'>React Semantic UI</SmartLink>. Blog entries are written in <SmartLink href='https://daringfireball.net/projects/markdown/syntax'>Markdown</SmartLink>, rendered with <SmartLink href='https://reactjs.org/'>React</SmartLink> components by <SmartLink href='https://github.com/rhysd/rehype-react'>rehype-react</SmartLink>. Code samples are highlighted using <SmartLink href='https://prismjs.com/'>Prism</SmartLink>. SVG graphics were produced using <SmartLink href='https://www.figma.com'>Figma</SmartLink>. Code was edited using <SmartLink href='https://code.visualstudio.com/'>VS Code</SmartLink>.
     </p>
     <p>
-      Most of the above are covered in blog entries to the right <Icon name='hand point right outline' color='violet'></Icon>
+      Most of the above are covered in <SmartLink href='/blog'>blog entries</SmartLink>.
     </p>
   </Layout>
 )
