4fdb19a6d79db79a97d0cb2d75448588c49613f6
trepidacious <trepidacious@gmail.com>
2019-03-24 10:38:21 +0000

    Import graphql to fix warning

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SmartLink from "../components/smart-link"
+import { graphql } from 'gatsby'
 
 export default ({ data }) => (
   <Layout>
